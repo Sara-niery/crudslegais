0f7634fbfd7a60d85711275334b1d57053b5ce40
Sara-niery <nierysantos@hotmail.com>
2022-08-30 17:24:11 -0300

    edição

--- core/forms.py
+++ core/forms.py
@@ -5,4 +5,4 @@ from .models import livro
 class LivroForm(ModelForm):
     class Meta:
         model = livro
-        fields = ['titulo', 'autor', 'ano']
+        fields = ['titulo', 'autor', 'edicao']
--- core/models.py
+++ core/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class livro(models.Model):
     titulo = models.CharField('Título', max_length=200)
     autor = models.CharField('Autor', max_length=100)
-    ano = models.IntegerField('Ano de Publicação')
+    edicao = models.IntegerField('Edição')
